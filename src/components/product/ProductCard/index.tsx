c0dea9d5d778a67469c128fbf92847955f917321
sona2711 <sonamkrtchyan2711@gmail.com>
2026-02-17 12:01:30 +0400

    update ProductCard

--- src/components/product/ProductCard/index.tsx
+++ src/components/product/ProductCard/index.tsx
@@ -1,4 +1,4 @@
-import { Card, Image, Button } from "antd";
+import { Card,  Button } from "antd";
 import { HeartOutlined, HeartFilled } from "@ant-design/icons";
 import { useTranslation } from "react-i18next";
 import type { ProductCardProps } from "./types";
@@ -18,13 +18,19 @@ export const ProductCard = ({item, onBook, isFavorite, onToggleFavorite}: Produc
       className={styles.card}
       cover={
         <div className={styles.coverWrapper}>
-          <Image
-          src={item.images[0]}
-          alt={item.name}
-          className={styles.image}
-          preview={false}
-          fallback="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='200' height='200'%3E%3Crect fill='%23ddd' width='200' height='200'/%3E%3Ctext fill='%23999' font-family='sans-serif' font-size='14' dy='10.5' font-weight='bold' x='50%25' y='50%25' text-anchor='middle'%3ENo Image%3C/text%3E%3C/svg%3E"
+          <img
+            src={item.images[0]}
+            alt={item.name}
+            className={`${styles.image} ${styles.primaryImage}`}
           />
+
+          {item.images[1] && (
+            <img
+            src={item.images[1]}
+            alt={item.name}
+            className={`${styles.image} ${styles.hoverImage}`}
+            />
+          )}
           <Button
             className={styles.favoriteBtn}
             type="text"
